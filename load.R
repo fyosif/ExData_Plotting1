7784e2108aef37b4e9ea2a85a263af5bb2fc5253
youseff <youseff@liveperson.com>
2015-09-09 15:50:31 +0300

    .

--- load.R
+++ load.R
@@ -1,5 +1,5 @@
-setwd("D:\YosifDir\coursera\exploratory  data\ExData_Plotting1")
-filedata <- "D:\YosifDir\coursera\exploratory  data\ExData_Plotting1\household_power_consumption.txt"
+setwd("D:\\YosifDir\\coursera\\exploratory  data\\ExData_Plotting1")
+filedata <- "D:\\YosifDir\\coursera\\exploratory  data\\ExData_Plotting1\\household_power_consumption.txt"
 meteredData <- read.table(filedata, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
 meteredData$Time <- strptime(paste(meteredData$Date, meteredData$Time), "%d/%m/%Y %H:%M:%S")
 meteredData$Date <- as.Date(meteredData$Date, "%d/%m/%Y")
